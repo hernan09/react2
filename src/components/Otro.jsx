ffe7d16365ed048cdab8b1bb7cd898af7b674a98
hernan <hernag_09@hotmail.com>
2019-02-28 20:54:03 -0300

    three commit

--- src/components/Otro.jsx
+++ src/components/Otro.jsx
@@ -6,8 +6,12 @@ class Otro extends Component{
     render(){
         return(
             <div id="otro">
-                <h2>Bienvenido a Warbringer</h2>
-                <p><strong>For</strong> the horde</p>
+                <h2>Bienvenido Warbringer</h2>
+                <blockquote class="blockquote text-center">
+                    <p class="mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+                    <footer class="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>
+                </blockquote>
+                <div id="div2"><p><small>This line of text is meant to be treated as fine print.</small></p></div>
 
             </div>
 
